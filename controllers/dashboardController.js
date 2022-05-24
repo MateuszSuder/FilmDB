4dd3e8dc3cf7a59415b25325f1eabfe6b32ca75f
Mateusz Suder <matisu999@gmail.com>
2022-05-24 22:35:34 +0200

    block dashboard from accesing without login

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,9 +1,21 @@
 import User from '../models/User.js'
 
 export const dashboardView = async (req, res) => {
-	// if(req.session.user?.permission !== 'admin') {
-	// 	return res.redirect('/');
-	// }
-	const users = await User.getAllUsers([], ['user', 'admin']);
+	if(req.session.user?.permission !== 'admin') {
+		return res.redirect('/');
+	}
+	const users = await User.getAllUsers([req.session.user.id], ['user', 'admin']);
 	res.render('layout', { page: 'views/dashboard', users });
+}
+
+export const modifyUserPermission = async (req, res) => {
+
+}
+
+export const deleteUser = async (req, res) => {
+
+}
+
+export const blockUser = async (req, res) => {
+
 }
